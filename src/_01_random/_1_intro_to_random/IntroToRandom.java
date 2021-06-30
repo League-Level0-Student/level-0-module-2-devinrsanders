34b95ff5714d02d61769243af375623c3b78e8c9
student <student@ip-172-31-58-42.us-west-2.compute.internal>
2021-06-29 18:26:45 -0700

    IntroToRandom RockPaperScissors(unfinished)

--- src/_01_random/_1_intro_to_random/IntroToRandom.java
+++ src/_01_random/_1_intro_to_random/IntroToRandom.java
@@ -30,7 +30,7 @@ public class IntroToRandom {
 		for (int i=0; i<5; i++) {
 			// Uncomment the line of code below, but replace the _____ with a positive integer (e.g. 100) before running the program			
 
-			// number = ran.nextInt(_____);
+			// number = ran.nextInt(100);
 			System.out.println(number);
 			
 		}
@@ -49,8 +49,8 @@ public class IntroToRandom {
 			// For our example, this will be:  ( 75 - 25 + 1 )
 			// Also replace <lowest value> with the start of the range.  For our example, this is 25.
 			
-			// number = ran.nextInt(_____) + <lowest value>;
-			System.out.println(number);
+			// number = ran.nextInt(75 - 25) + <lowest integer>;
+			System.out.println();
 			
 		}
 		// This time, all the values printed should be between 25 and 75.
@@ -59,6 +59,10 @@ public class IntroToRandom {
 		/*************  Random integers with an upper and lower (negative) limit  *************/
 		
 		// Using the above example, generate 5 random numbers between -222 and 88
+		for (int i=0; i<5; i++) {
+			number = ran.nextInt(222 - 88) + 88;
+			System.out.println(number);
+		}
 
 
 	}
